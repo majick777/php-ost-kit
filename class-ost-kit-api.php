c42df408bea479ee2987391c924deebb9706ddb6
Tony Hayes <majick777@gmail.com>
2018-05-10 14:19:56 +1000

    added license and version

--- class-ost-kit-api.php
+++ class-ost-kit-api.php
@@ -4,8 +4,26 @@
 // === OST KIT alpha API ===
 // === PHP Wrapper Class ===
 // =========================
-// ===== Version 1.0.0 =====
+// ===== Version 092.1 =====
+// == rev 1 for API 0.9.2 ==
 // =========================
+//
+// OST KITa API PHP Wrapper Class
+// Copyright (C) 2018 Tony Hayes
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
 //
 // Usage Note: define constants before instantiating and/or arguments while instantiating.
 // ---------------------------------------------------------------------------------------
